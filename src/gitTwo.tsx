d5d71048497e9c101592e52cde6ce16ed97d0fdf
unknown <siddhant.singh5120@yahoo.in>
2021-09-13 10:25:36 +0530

    Added two files

---
+++ src/gitTwo.tsx
@@ -0,0 +1,12 @@
+import React, { useCallback, useEffect, useState } from 'react';
+
+
+function TestingTree (){
+    return(
+        <div>
+            hello world
+        </div>
+    )
+}
+
+export default TestingTree;
---
+++ src/gitone.tsx
@@ -0,0 +1,12 @@
+import React, { useCallback, useEffect, useState } from 'react';
+
+
+function Testing (){
+    return(
+        <div>
+            hello world
+        </div>
+    )
+}
+
+export default Testing;
